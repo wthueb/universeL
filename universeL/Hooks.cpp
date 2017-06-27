144cd5da43e9760a441222fddbad4d16fa4f4ca5
William Huebner <wilhueb@gmail.com>
2017-06-27 02:15:15 -0400

    fixed version number

--- universeL/Hooks.cpp
+++ universeL/Hooks.cpp
@@ -14,7 +14,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 11
-#define VERSION_PATCH 1
+#define VERSION_PATCH 2
 
 extern LRESULT ImGui_ImplDX9_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
